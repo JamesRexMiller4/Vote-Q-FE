ee26554503e42b4c86e2430423e67dc51da4b68d
James Miller <james.rex.miller.4@gmail.com>
2020-06-14 14:37:29 -0600

    Connect component toApp state, add favicon

--- src/components/votingcenter/CenterDetail.js
+++ src/components/votingcenter/CenterDetail.js
@@ -1,14 +1,18 @@
 import React, {useContext} from "react";
 import QueueContainer from "../queue/QueueContainer";
 import { AppContext } from "../app/App";
+import { Link } from 'react-router-dom';
 
 function CenterDetail() {
   const {state} = useContext(AppContext);
-
+  console.log(state)
   return (
     <div>
       <h1>{state.selectedCenter.name}</h1>
       <QueueContainer/>
+      <Link to={'/voting-centers/0/confirmation'}>
+        <button>Click</button>
+      </Link>
     </div>
   )
 }
